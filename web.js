72fd936ca564b4e056a0082ca538fbb577ca8a5a
Pawel Moczyrog <pawelm@post.pl>
2013-07-06 22:09:19 +0000

    test2

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buffer = new Buffer();
+  var buffer = new Buffer(1024);
   
   response.send(buffer.toString("utf-8", fs.readFileSync("index.html")));
 
