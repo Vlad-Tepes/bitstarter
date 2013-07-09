05eeab0c0f68624ef77a85c4036c29ff65eb2960
Pawel Moczyrog <pawelm@post.pl>
2013-07-09 17:29:48 +0000

    Initial test of grader

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,8 +45,12 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfile, checksfile, mode) {
+    if(mode =="file") {
+        $ = cheerioHtmlFile(htmlfile);
+    } else {
+	$ = cheerio.load(htmlfile);
+    }
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -63,10 +68,27 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to html file') 
+       .parse(process.argv);
+    if(program.url) {
+	//console.log("URL");
+	rest.get(program.url).on('complete', function(result) {
+		if (result instanceof Error) {
+			console.log("Error!");
+		} else {
+			//console.log(result);
+			var chekJson = checkHtmlFile(result, program.checks, "url");
+		}
+	})
+	//var checkJson = checkHtmlFile(program.url, program.checks);
+    } else {
+        if(program.file) {
+	    //console.log("FILE")
+	    var checkJson = checkHtmlFile(program.file, program.checks, "file");
+	}
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
